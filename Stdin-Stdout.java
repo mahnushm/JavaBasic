61a8e440ce941d4405b4ee9bbde67fd5b2daf87d
Mahnush Movahedi <mahnush.movahedi@yale.edu>
2016-10-31 18:48:14 -0400

    Update Stdin-Stdout.java

--- Stdin-Stdout.java
+++ Stdin-Stdout.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
+\\ A Scanner breaks its input into tokens using a delimiter pattern, which by default matches whitespace. 
+\\The resulting tokens may then be converted into values of different types using the various next methods.
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         System.out.println(a);
